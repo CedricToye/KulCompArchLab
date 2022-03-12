0923cbbda7281b6d95d5f0e7f3eb5f6cbf8aaf44
CedricToye <99961034+CedricToye@users.noreply.github.com>
2022-03-12 17:02:57 +0100

    I'm a cucumber

--- app/src/main.c
+++ app/src/main.c
@@ -2,9 +2,116 @@
 #include <stm32l4xx.h>
 // Cédric Toye
 
+int mux = 0;
+int uren = 0;
+int minuten = 0;
+int ms = 0;
 
-void SystickHandler(void) {
-	//Routine
+void SysTick_Handler(void) {
+	switch(mux){
+		case 0:
+			clear();
+			GPIOA->ODR &= ~GPIO_ODR_OD8;
+			GPIOA->ODR &= ~GPIO_ODR_OD15;
+			segments(uren/10);
+			break;
+		case 1:
+			clear();
+			GPIOA->ODR |= GPIO_ODR_OD6;
+			GPIOA->ODR |= GPIO_ODR_OD8;
+			GPIOA->ODR &= ~GPIO_ODR_OD15;
+			segments(uren%10);
+			break;
+		case 2:
+			clear();
+			GPIOA->ODR &= ~GPIO_ODR_OD8;
+			GPIOA->ODR |= GPIO_ODR_OD15;
+			segments(minuten/10);
+			break;
+		case 3:
+			clear();
+			GPIOA->ODR |= GPIO_ODR_OD8;
+			GPIOA->ODR |= GPIO_ODR_OD15;
+			segments(minuten%10);
+			break;
+	}
+
+	mux++;
+	ms++;
+
+	if(mux > 3){
+		mux = 0;
+	}
+
+	if(ms == 1000){
+		ms = 0;
+		minuten++;
+		if(minuten > 59){
+			minuten = 0;
+			uren++;
+			if(uren > 23){
+				uren = 0;
+			}
+		}
+	}
+
+}
+
+void clear(void){
+	GPIOA->ODR &= ~(GPIO_ODR_OD7|GPIO_ODR_OD5|GPIO_ODR_OD6);
+	GPIOB->ODR &= ~(GPIO_ODR_OD0|GPIO_ODR_OD12|GPIO_ODR_OD15|GPIO_ODR_OD1|GPIO_ODR_OD2);
+}
+
+void segments(unsigned int n){
+	switch(n){
+		case 0:
+			GPIOA->ODR |= (GPIO_ODR_OD5|GPIO_ODR_OD7);
+			GPIOB->ODR |= (GPIO_ODR_OD0|GPIO_ODR_OD1|GPIO_ODR_OD12|GPIO_ODR_OD15);
+			break;
+
+		case 1:
+			GPIOA->ODR |= (GPIO_ODR_OD5|GPIO_ODR_OD7);
+			break;
+
+		case 2:
+			GPIOA->ODR |= (GPIO_ODR_OD7);
+			GPIOB->ODR |= (GPIO_ODR_OD0|GPIO_ODR_OD2|GPIO_ODR_OD12|GPIO_ODR_OD15);
+			break;
+
+		case 3:
+			GPIOA->ODR |= (GPIO_ODR_OD5|GPIO_ODR_OD7);
+			GPIOB->ODR |= (GPIO_ODR_OD0|GPIO_ODR_OD2|GPIO_ODR_OD12);
+			break;
+
+		case 4:
+			GPIOA->ODR |= (GPIO_ODR_OD5|GPIO_ODR_OD7);
+			GPIOB->ODR |= (GPIO_ODR_OD1|GPIO_ODR_OD2);
+			break;
+
+		case 5:
+			GPIOA->ODR |= (GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0|GPIO_ODR_OD1|GPIO_ODR_OD2|GPIO_ODR_OD12);
+			break;
+
+		case 6:
+			GPIOA->ODR |= GPIO_ODR_OD5;
+			GPIOB->ODR |= (GPIO_ODR_OD0|GPIO_ODR_OD1|GPIO_ODR_OD2|GPIO_ODR_OD12);
+			break;
+
+		case 7:
+			GPIOA->ODR |= (GPIO_ODR_OD5|GPIO_ODR_OD7);
+			GPIOB->ODR |= GPIO_ODR_OD0;
+			break;
+		case 8:
+			GPIOA->ODR |= (GPIO_ODR_OD5|GPIO_ODR_OD7);
+			GPIOB->ODR |= (GPIO_ODR_OD0|GPIO_ODR_OD1|GPIO_ODR_OD2|GPIO_ODR_OD12|GPIO_ODR_OD15);
+			break;
+
+		case 9:
+			GPIOA->ODR |= (GPIO_ODR_OD5|GPIO_ODR_OD7);
+			GPIOB->ODR |= (GPIO_ODR_OD0|GPIO_ODR_OD1|GPIO_ODR_OD2|GPIO_ODR_OD12);
+			break;
+	}
 }
 
 int main(void) {
@@ -14,56 +121,36 @@ int main(void) {
 	NVIC_SetPriority(SysTick_IRQn, 128);
 	NVIC_EnableIRQ(SysTick_IRQn);
 
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN_Msk; // Activeren klok blok A
-	RCC->AHB2ENR |= rcc_AHB2ENR_GPIOBEN_Msk; // Activeren klok blok B
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN_Msk; // Activating clock block A
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN_Msk; // Activating clock block B
 
 
-	//instellen van 7seg displays
-	GPIOA->MODER &= ~(GPIO_MODER_MODE8_Msk|GPIO_MODER_MODE15_Msk); //bitwise and operation of GPIOA MODER register and mask
-	GPIOA->MODER |= (GPIO_MODER_MODE8_0|GPIO_MODER_MODE15_0); //bitwise or operation of GPIOA MODER register and 01 bits for pin 8 and pin 15
+	//setting multiplexer
+	GPIOA->MODER &= ~(GPIO_MODER_MODE8_Msk|GPIO_MODER_MODE15_Msk); // bitwise and operation of GPIOA MODER register and mask
+	GPIOA->MODER |= (GPIO_MODER_MODE8_0|GPIO_MODER_MODE15_0); // bitwise or operation of GPIOA MODER register and 01 bits for pin 8 and pin 15
 	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT8|GPIO_OTYPER_OT15);
 
-	//instellen van segmenten
-	GPIOA -> MODER &= ~(GPIO_MODER_MODE5_Msk|GPIO_MODER_MODE7_Msk);
-	GPIOA -> MODER |= (GPIO_MODER_MODE5_0|GPIO_MODER_MODE7_0);
-	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT5|GPIO_OTYPER_OT7);
+	//setting segments
+	GPIOA->MODER &= ~(GPIO_MODER_MODE5_Msk|GPIO_MODER_MODE6_Msk|GPIO_MODER_MODE7_Msk); // 7-B, 5-C, 6-DP
+	GPIOA->MODER |= (GPIO_MODER_MODE5_0|GPIO_MODER_MODE6_0|GPIO_MODER_MODE7_0);
+	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT5|GPIO_OTYPER_OT6|GPIO_OTYPER_OT7);
 
 
-	GPIOB->MODER &= ~(GPIO_MODER_MODE0_Msk|GPIO_MODER_MODE1_Msk|GPIO_MODER_MODE2_Msk|GPIO_MODER_MODE12_Msk|GPIO_MODER_MODE15_Msk);
+	GPIOB->MODER &= ~(GPIO_MODER_MODE0_Msk|GPIO_MODER_MODE1_Msk|GPIO_MODER_MODE2_Msk|GPIO_MODER_MODE12_Msk|GPIO_MODER_MODE15_Msk); // 0-A, 12-D, 15-E, 1-F, 2-G
 	GPIOB->MODER |= (GPIO_MODER_MODE0_0|GPIO_MODER_MODE1_0|GPIO_MODER_MODE2_0|GPIO_MODER_MODE12_0|GPIO_MODER_MODE15_0);
 	GPIOB->OTYPER &= ~(GPIO_OTYPER_OT0|GPIO_OTYPER_OT1|GPIO_OTYPER_OT2|GPIO_OTYPER_OT12|GPIO_OTYPER_OT15);
 
-	//instellen van
+	//setting buttons
+	GPIOB->MODER &= ~GPIO_MODER_MODE13_Msk; // Button A
+	GPIOB->MODER &= ~GPIO_MODER_MODE14_Msk; // Button B
 
-	int uren = 0;
-	int minuten = 0;
-
-    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
-    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
-
-
-    //Mux + DP
-    GPIOA->MODER &= ~(GPIO_MODER_MODE8_Msk|GPIO_MODER_MODE15_Msk|GPIO_MODER_MODE6_Msk);
-    GPIOA->MODER |= (GPIO_MODER_MODE8_0 | GPIO_MODER_MODE15_0 | GPIO_MODER_MODE6_0);
-    GPIOA->OTYPER &= ~(GPIO_OTYPER_OT8|GPIO_OTYPER_OT15|GPIO_OTYPER_OT6);
-
-    //7 segment display
-    GPIOA->MODER &= ~(GPIO_MODER_MODE8_Msk|GPIO_MODER_MODE15_Msk|GPIO_MODER_MODE6_Msk);
-    GPIOA->MODER |= (GPIO_MODER_MODE8_0 | GPIO_MODER_MODE15_0 | GPIO_MODER_MODE6_0);
-    GPIOA->OTYPER &= ~(GPIO_OTYPER_OT8|GPIO_OTYPER_OT15|GPIO_OTYPER_OT6);
-
-    //KnopA
-    GPIOB->MODER &= ~GPIO_MODER_MODE13_Msk;
-
-    //KnopB
-    GPIOB->MODER &= ~GPIO_MODER_MODE14_Msk;
-
-    //pull up weerstand hoog zetten
-    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD13_Msk;
-    GPIOB->PUPDR |= GPIO_PUPDR_PUPD13_0;
-    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD14_Msk;
-    GPIOB->PUPDR |= GPIO_PUPDR_PUPD14_0;
+	//setting pull up resistors
+	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD13_Msk;
+	GPIOB->PUPDR |= GPIO_PUPDR_PUPD13_0;
+	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD14_Msk;
+	GPIOB->PUPDR |= GPIO_PUPDR_PUPD14_0;
 
 	while (1) {
+
 	}
 }
