cf06a3cb73f626a08cf20434ba9b10767020bb80
CedricToye <99961034+CedricToye@users.noreply.github.com>
2022-04-29 11:43:02 +0200

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -8,7 +8,6 @@ float V;
 float R;
 
 float value_NTC;
-float value_POT;
 
 void delay(unsigned int n) {
 	volatile unsigned int delay = n;
@@ -57,15 +56,6 @@ void clear(void) {
 	GPIOA->ODR &= ~(GPIO_ODR_OD7 | GPIO_ODR_OD5 | GPIO_ODR_OD6);
 	GPIOB->ODR &= ~(GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1
 			| GPIO_ODR_OD2);
-}
-
-void limit(void){
-	if (value_NTC < value_POT){
-		TIM16->BDTR |= TIM_BDTR_MOE;
-	}
-	else{
-		TIM16->BDTR &= ~TIM_BDTR_MOE;
-	}
 }
 
 void segments(unsigned int n) {
@@ -136,27 +126,10 @@ int main(void) {
 	RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN; // Activating clock ADC
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN_Msk; // Activating clock block A
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN_Msk; // Activating clock block B
-	RCC->APB2ENR |= RCC_APB2ENR_TIM16EN;
 
 	// Klok selecteren
 	RCC->CCIPR &= ~RCC_CCIPR_ADCSEL_Msk;
 	RCC->CCIPR |= (RCC_CCIPR_ADCSEL_0 | RCC_CCIPR_ADCSEL_1);
-
-	//Timer
-	GPIOB->MODER &= ~GPIO_MODER_MODE8_Msk;
-	GPIOB->MODER |=  GPIO_MODER_MODE8_1;
-	GPIOB->OTYPER &= ~GPIO_OTYPER_OT8;
-	GPIOB->AFR[1] = (GPIOB->AFR[1] & (~GPIO_AFRH_AFSEL8_Msk)) | (0xE << GPIO_AFRH_AFSEL8_Pos);
-
-	TIM16->PSC = 0;
-	TIM16->ARR = 24000;
-	TIM16->CCR1 = 12000;
-
-	TIM16->CCMR1 &= ~TIM_CCMR1_CC1S;
-	TIM16->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1FE;
-	TIM16->CCER |= TIM_CCER_CC1E;
-	TIM16->CCER &= ~TIM_CCER_CC1P;
-	TIM16->CR1 |= TIM_CR1_CEN;
 
 	// Deep powerdown modus uitzetten
 	ADC1->CR &= ~ADC_CR_DEEPPWD;
@@ -176,7 +149,8 @@ int main(void) {
 
 	// Kanalen instellen
 	ADC1->SMPR1 |= (ADC_SMPR1_SMP5_0 | ADC_SMPR1_SMP5_1 | ADC_SMPR1_SMP5_2);
-	ADC1->SMPR1 |= (ADC_SMPR1_SMP6_0 | ADC_SMPR1_SMP6_1 | ADC_SMPR1_SMP6_2);
+	// kanaal instellen
+	ADC1->SQR1 |= (ADC_SQR1_SQ1_0 | ADC_SQR1_SQ1_2);
 
 	//Setting NTC as analog
 	GPIOA->MODER &= ~GPIO_MODER_MODE0_Msk; // bits op 0 zetten
@@ -203,11 +177,7 @@ int main(void) {
 			| GPIO_OTYPER_OT12 | GPIO_OTYPER_OT15);
 
 	while (1) {
-		 // Start de ADC en wacht tot de sequentie klaar is
-
-		 // kanaal instellen
-		 ADC1->SQR1 &= ~(ADC_SQR1_SQ1_0 | ADC_SQR1_SQ1_1 | ADC_SQR1_SQ1_2);
-		 ADC1->SQR1 |= (ADC_SQR1_SQ1_0 | ADC_SQR1_SQ1_2);
+		 // Start de ADC en wacht tot de omzetting klaar is
 		 ADC1->CR |= ADC_CR_ADSTART;
 		 while(!(ADC1->ISR & ADC_ISR_EOC));
 
@@ -219,19 +189,5 @@ int main(void) {
 		 temperatuur = 10*((1.0f/((logf(R/10000.0f)/3936.0f)+(1.0f/298.15f)))-273.15f);
 
 		 delay(1000000);
-
-		 //Kanaal instellen
-		 ADC1->SQR1 &= ~(ADC_SQR1_SQ1_0 | ADC_SQR1_SQ1_1 | ADC_SQR1_SQ1_2);
-		 ADC1->SQR1 |= (ADC_SQR1_SQ1_1 | ADC_SQR1_SQ1_2);
-		 ADC1->CR |= ADC_CR_ADSTART;
-		 while(!(ADC1->ISR & ADC_ISR_EOC));
-
-		 //Waarde inlezen
-		 value_POT = ADC1->DR;
-
-		 delay(1000000);
-
-		 limit();
-
 	}
 }
