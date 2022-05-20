413eb3bd3270f03274bba53cecef695be71484e7
CedricToye <99961034+CedricToye@users.noreply.github.com>
2022-05-20 11:10:30 +0200

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -64,7 +64,6 @@ int main(void) {
 	NVIC_SetPriority(SysTick_IRQn, 128);
 	NVIC_EnableIRQ(SysTick_IRQn);
 
-	RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN; // Activating clock ADC
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN_Msk; // Activating clock block A
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN_Msk; // Activating clock block B
 	RCC->APB1ENR1 |= RCC_APB1ENR1_I2C1EN; // Activating clock I2C
@@ -72,30 +71,6 @@ int main(void) {
 	// Klok selecteren voor ADC
 	RCC->CCIPR &= ~RCC_CCIPR_ADCSEL_Msk;
 	RCC->CCIPR |= (RCC_CCIPR_ADCSEL_0 | RCC_CCIPR_ADCSEL_1);
-
-	// Deep powerdown modus uitzetten
-	ADC1->CR &= ~ADC_CR_DEEPPWD;
-
-	// ADC voltage regulator aanzetten
-	ADC1->CR |= ADC_CR_ADVREGEN;
-
-	// Delay 20 microseconds
-	delay(1500);
-
-	// Kalibratie starten
-	ADC1->CR |= ADC_CR_ADCAL;
-	while(ADC1->CR & ADC_CR_ADCAL);
-
-	//ADC aanzetten
-	ADC1->CR |= ADC_CR_ADEN;
-
-	// Kanalen instellen
-	ADC1->SMPR1 |= (ADC_SMPR1_SMP5_0 | ADC_SMPR1_SMP5_1 | ADC_SMPR1_SMP5_2);
-	ADC1->SQR1 |= (ADC_SQR1_SQ1_0 | ADC_SQR1_SQ1_2);
-
-	//Setting NTC as analog
-	GPIOA->MODER &= ~GPIO_MODER_MODE0_Msk; // bits op 0 zetten
-	GPIOA->MODER |= GPIO_MODER_MODE0_0 | GPIO_MODER_MODE0_1; // Bit 0 en 1 hoog zetten voor analoge modus
 
 	//Setting GPIO
 	GPIOB->MODER &= ~GPIO_MODER_MODE6_Msk;
@@ -112,6 +87,21 @@ int main(void) {
 	I2C1->TIMINGR = 0x20303E5D;
 	I2C1->CR2 |= (I2C_CR2_AUTOEND | I2C_CR2_NACK);
 	I2C1->CR1 |= I2C_CR1_PE;
+
+	//setting segments
+	GPIOA->MODER &= ~(GPIO_MODER_MODE5_Msk | GPIO_MODER_MODE6_Msk
+			| GPIO_MODER_MODE7_Msk); // 7-B, 5-C, 6-DP
+	GPIOA->MODER |= (GPIO_MODER_MODE5_0 | GPIO_MODER_MODE6_0
+			| GPIO_MODER_MODE7_0);
+	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT5 | GPIO_OTYPER_OT6 | GPIO_OTYPER_OT7);
+
+	GPIOB->MODER &= ~(GPIO_MODER_MODE0_Msk | GPIO_MODER_MODE1_Msk
+			| GPIO_MODER_MODE2_Msk | GPIO_MODER_MODE12_Msk
+			| GPIO_MODER_MODE15_Msk); // 0-A, 12-D, 15-E, 1-F, 2-G
+	GPIOB->MODER |= (GPIO_MODER_MODE0_0 | GPIO_MODER_MODE1_0
+			| GPIO_MODER_MODE2_0 | GPIO_MODER_MODE12_0 | GPIO_MODER_MODE15_0);
+	GPIOB->OTYPER &= ~(GPIO_OTYPER_OT0 | GPIO_OTYPER_OT1 | GPIO_OTYPER_OT2
+			| GPIO_OTYPER_OT12 | GPIO_OTYPER_OT15);
 
 	while (1) {
 
